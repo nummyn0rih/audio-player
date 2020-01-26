720e8dbe25a538e2ce5b401288fd996927aeb753
nummyn0rih <nummyn0rih@gmail.com>
2020-01-27 02:42:52 +0300

    add pug

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const pug = require('gulp-pug');
 
 const paths = {
   out: './dist',
@@ -44,6 +45,35 @@ function browserSync(done) {
 // Clean dist
 function clean() {
   return del(['./dist/']);
+}
+
+// Compile Pug
+function pugbuild() {
+  return gulp
+    .src('./app/common.blocks/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./app/common.blocks/'))
+    .pipe(browsersync.stream());
+}
+
+function htmlbeautify() {
+  const options = {
+    indentSize: 2,
+    unformatted: [
+      // https://www.w3.org/TR/html5/dom.html#phrasing-content
+      'abbr', 'area', 'b', 'bdi', 'bdo', 'br', 'cite',
+      'code', 'data', 'datalist', 'del', 'dfn', 'em', 'embed', 'i', 'ins',
+      'kbd', 'keygen', 'map', 'mark', 'math', 'meter', 'noscript',
+      'object', 'output', 'progress', 'q', 'ruby', 's', 'samp', 'small',
+      'strong', 'sub', 'sup', 'template', 'time', 'u', 'var', 'wbr', 'text',
+      'acronym', 'address', 'big', 'dt', 'ins', 'strike', 'tt',
+    ],
+  };
+
+  return gulp
+    .src('./dist/pug/page/*.html')
+    .pipe(htmlbeautify(options))
+    .pipe(gulp.dest('./dist/beautyfy'));
 }
 
 // HTML task
@@ -119,6 +149,8 @@ const build = gulp.series(clean, gulp.parallel(html, css, images, fonts, js));
 const watch = gulp.parallel(watchFiles, browserSync);
 
 // export tasks
+exports.pugbuild = pugbuild;
+exports.htmlbeautify = htmlbeautify;
 exports.css = css;
 exports.js = js;
 exports.images = images;

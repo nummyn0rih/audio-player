c163593e8bc2d5e5d5b11e211be30bb0db9120da
nummyn0rih <nummyn0rih@gmail.com>
2020-01-27 03:38:44 +0300

    added change of progress

--- app/common.blocks/track/track.js
+++ app/common.blocks/track/track.js
@@ -6,9 +6,22 @@ document.addEventListener('click', (event) => {
   if (!track) return;
   if (event.target.className === 'star') return;
 
-  const trackList = [...document.querySelectorAll('.track')];
-  trackList.map((item) => item.classList.remove('track_active'));
-  track.classList.toggle('track_active');
+  const progressbar = track.querySelector('.track__progress');
+
+  if (track.classList.contains('track_active')) {
+    const progressbarCoord = track.getBoundingClientRect();
+    const startOfProgress = progressbarCoord.x;
+    const progressbarWidht = progressbarCoord.width;
+    const curentCoord = event.clientX;
+    // eslint-disable-next-line no-mixed-operators
+    const newProgress = 100 - Math.round((curentCoord - startOfProgress) / progressbarWidht * 100);
+    progressbar.style.right = `${newProgress}%`;
+  } else {
+    const trackList = [...document.querySelectorAll('.track')];
+    trackList.map((item) => item.classList.remove('track_active'));
+    track.classList.toggle('track_active');
+    progressbar.style.right = '99%';
+  }
 
   event.preventDefault();
 });
